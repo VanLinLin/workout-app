285f389d2aeb9d90ad7a85132810d9d6621c2848
VanLinLin <aas12as12as12tw@gmail.com>
2022-08-27 21:44:10 +0800

    rename pushup to pushup_ui_implement

--- pushup_ui_implement.py
+++ pushup_ui_implement.py
@@ -1,13 +1,12 @@
-from PySide6 import QtWidgets, QtGui, QtCore
-from UI_implement.Ui_workout import Ui_MainWindow
+from PySide6 import QtWidgets
+from UI_implement.Ui_pushup import Ui_PushUP
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         # in python3, super(Class, self).xxx = super().xxx
         super().__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_PushUP()
         self.ui.setupUi(self)
-
 
 if __name__ == '__main__':
     import sys

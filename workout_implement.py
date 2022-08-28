7187381560c5377cbfe75fd5fed262ea490d2209
VanLinLin <aas12as12as12tw@gmail.com>
2022-08-28 09:22:07 +0800

    create the open pushup window function

--- workout_implement.py
+++ workout_implement.py
@@ -13,14 +13,18 @@ class WorkoutMainWindow(QtWidgets.QMainWindow):
         # setup gif 
         self.setup_gif()
 
-        # Pushup window setting
-        self.pushup_window = PushupMainWindow("伏地挺身")
-        self.workout_window.push_up_button.clicked.connect(self.pushup_window.show)
+        # pushup window setting
+        self.open_pushup_window(window_title="伏地挺身")
+
         
     def setup_gif(self) -> None:
         self.john_cena_gif: QMovie = QMovie('sources\gif\john_cena.gif')
         self.workout_window.john_cena.setMovie(self.john_cena_gif)
         self.john_cena_gif.start()
+
+    def open_pushup_window(self, window_title="伏地挺身"):
+        self.pushup_window = PushupMainWindow(window_title)
+        self.workout_window.push_up_button.clicked.connect(self.pushup_window.show)
 
 
 if __name__ == '__main__':

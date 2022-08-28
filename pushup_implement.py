bcd0d34b2a47a36644d706af415ca3f8d4f21605
VanLinLin <aas12as12as12tw@gmail.com>
2022-08-28 09:14:52 +0800

    add parameter to set the pushup window title

--- pushup_implement.py
+++ pushup_implement.py
@@ -1,16 +1,17 @@
 from PySide6 import QtWidgets
 from UI_implement.Ui_pushup import Ui_PushUP
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+class PushupMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, window_title):
         # in python3, super(Class, self).xxx = super().xxx
         super().__init__()
         self.ui = Ui_PushUP()
         self.ui.setupUi(self)
+        self.setWindowTitle(window_title)
 
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    window = PushupMainWindow()
     window.show()
     sys.exit(app.exec())

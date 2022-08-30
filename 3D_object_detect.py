1f91e0dc4274829c57c1e9c11f5713a37fca9da3
VanLinLin <aas12as12as12tw@gmail.com>
2022-08-30 18:44:20 +0800

    update 3D object detect and hand tracking file

--- 3D_object_detect.py
+++ 3D_object_detect.py
@@ -2,57 +2,92 @@ import cv2
 import mediapipe as mp
 import time
 
-# 載入物件辨識所需權重
+# 匯入物件辨識所需權重
 mp_objectron = mp.solutions.objectron
+
+# 匯入繪圖風格套件
+mp_drawing_styles = mp.solutions.drawing_styles
+
+# 匯入繪圖方法
 mp_drawing = mp.solutions.drawing_utils
 
 # 建立鏡頭物件
 cap = cv2.VideoCapture(1)
 
-# 建立物件
-with mp_objectron.Objectron(static_image_mode=False,
-                            max_num_objects=5,
-                            min_detection_confidence=0.2,
-                            min_tracking_confidence=0.5,
-                            model_name='Cup'
+# 建立3D物件辨識的物件
+with mp_objectron.Objectron(
+                            static_image_mode=False,        # 偵測照片時才需開啟
+                            max_num_objects=5,              # 最大物件數量
+                            min_detection_confidence=0.2,   # 最小偵測信心程度，小於此值將直接忽略
+                            min_tracking_confidence=0.5,    # 最小追蹤信心程度，小於此值將直接忽略
+                            model_name='Shoe'                # 要偵測的物件
                             ) as objectron:
     
-    # 開始讀取影像
+    # 如果鏡頭開啟，則進入迴圈
     while cap.isOpened():
         
+        # success -> 使用影片才會用到，用於判斷影片是否結束
+        # image -> 每一幀影像
         success, image = cap.read()
 
-        start = time.time()
-
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        image.flags.writeable = False
-
-        result = objectron.process(image)
-
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-
-        image.flags.writeable = True
-
-        if result.detected_objects:
-            for detected_object in result.detected_objects:
-                mp_drawing.draw_landmarks(image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
-                mp_drawing.draw_axis(image, detected_object.rotation, detected_object.translation)
-
-
-        end = time.time()
-
-        total_time = end - start
-
-        fps = 1 / total_time
-
-        cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
-
-        cv2.imshow('3D object detection', image)
-
-        if cv2.waitKey(5) == ord('q'):
+        # 若是沒有串流資源，則離開迴圈
+        if not success:
             break
 
+        # 每一幀影像開始處理時間
+        start = time.time()
 
+        # 轉換色彩通道順序
+        # openCV -> BGR，mediapipe偵測使用 RGB，因此需轉換
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # 一開始偵測的影像不進行繪圖，提高運算速度
+        image.flags.writeable = False
+
+        # mediapipe 對每一幀影像進行處理
+        result = objectron.process(image)
+
+        # 因為顯示是使用openCV，因此需再將色彩通道轉回來
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # 因為要看到效果，因此要把繪圖功能打開
+        image.flags.writeable = True
+
+        # 判斷是否有偵測到物件
+        if result.detected_objects:
+
+            # 將偵測到的每一個物件取出
+            for detected_object in result.detected_objects:
+
+                # 畫出地標
+                mp_drawing.draw_landmarks(
+                                            image, 
+                                            detected_object.landmarks_2d, 
+                                            mp_objectron.BOX_CONNECTIONS,
+                                            )
+
+                # 畫出軸
+                mp_drawing.draw_axis(image, detected_object.rotation, detected_object.translation)
+
+        # 每一幀影像處理結束時間
+        end = time.time()
+
+        # 處理每一幀影像時間間隔
+        total_time = end - start
+
+        # 計算fps
+        fps = 1 / total_time
+
+        # 用於將文字顯示在影像上
+        cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
+
+        # 顯示影像
+        cv2.imshow('3D object detection', image)
+
+        # 判斷q是否被按下(不分大小寫)
+        if cv2.waitKey(5) == ord('q'.casefold()):
+            # q被按下 -> 離開迴圈
+            break
+
+# 釋放鏡頭資源
 cap.release()
--- solutions/hand_tracking.py
+++ solutions/hand_tracking.py
@@ -1,36 +1,75 @@
 import cv2
 import mediapipe as mp
+
+# 匯入繪圖套件
 mp_drawing = mp.solutions.drawing_utils
+
+# 匯入繪圖風格套件
 mp_drawing_styles = mp.solutions.drawing_styles
+
+# 匯入手部辨識權重
 mp_hands = mp.solutions.hands
 
-
+# 判斷是否有其他鏡頭資源，若沒有的話則使用原本的鏡頭
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW) if cv2.VideoCapture(1).isOpened() else cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+# 建立手部辨識物件
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      break
+                    model_complexity=0,             # 模型複雜度，範圍:1~2，越大越複雜，判斷時間越久、但越準確
+                    min_detection_confidence=0.5,   # 最小偵測信心程度，小於此值將直接忽略
+                    min_tracking_confidence=0.5     # 最小追蹤信心程度，小於此值將直接忽略
+                    ) as hands:
 
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    # 如果鏡頭開啟，則進入迴圈
+    while cap.isOpened():
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) == ord('q'):
-      break
-cap.release()
+        # success -> 使用影片才會用到，用於判斷影片是否結束
+        # image -> 每一幀影像
+        success, image = cap.read()
+        
+        # 若是沒有串流資源，則離開迴圈
+        if not success:
+            break
+
+        # 一開始偵測的影像不進行繪圖，提高運算速度
+        image.flags.writeable = False
+
+
+        # 轉換色彩通道順序
+        # openCV -> BGR，mediapipe偵測使用 RGB，因此需轉換
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # mediapipe 對每一幀影像進行處理
+        results = hands.process(image)
+
+
+        # 因為要看到效果，因此要把繪圖功能打開
+        image.flags.writeable = True
+
+        # 因為顯示是使用openCV，因此需再將色彩通道轉回來
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # 判斷是否有偵測到物件
+        if results.multi_hand_landmarks:
+            # 將偵測到的每一個物件取出
+            for hand_landmarks in results.multi_hand_landmarks:
+
+                # 畫出地標
+                mp_drawing.draw_landmarks(
+                                            image,
+                                            hand_landmarks,
+                                            mp_hands.HAND_CONNECTIONS,
+                                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                                            mp_drawing_styles.get_default_hand_connections_style()
+                                            )
+
+        # 顯示影像
+        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+
+        # 判斷q是否被按下(不分大小寫)
+        if cv2.waitKey(5) == ord('q'):
+            # q被按下 -> 離開迴圈
+            break
+
+    # 釋放鏡頭資源
+    cap.release()
